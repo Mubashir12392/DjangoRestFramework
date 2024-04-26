ddb8c6c9f6efdcb1e42205b6ccbb547d45bdc16f
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-27 00:18:02 +0500

    create a model name Item

--- basicCrud/models.py
+++ basicCrud/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    
+    name = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name
