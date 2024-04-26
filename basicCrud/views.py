f1ea58e775d8a48b6c781ec4e637d6d38a834780
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-27 00:19:30 +0500

    Send the Model serialized data as a response to Get Request

--- basicCrud/views.py
+++ basicCrud/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .models import Item
+from .serializers import ItemSerializer
+
 # Create your views here.
+
+# -----CRUD (Read)-----
+
 @api_view(['GET'])
 def getData(request):
-    person = {'name':'Mubashir',
-              'age':20}
-    return Response(person)
+    
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
