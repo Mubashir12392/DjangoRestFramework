7310acee787dbaa9c1d6d21b50d986ae23d1068f
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-27 15:46:28 +0500

    1. Add some new fields in Item Model

--- basicCrud/models.py
+++ basicCrud/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class Item(models.Model):
     
     name = models.CharField(max_length=50)
+    company = models.CharField(max_length=50, null=True)
+    owner = models.CharField(max_length=50, null=True)
     created_at = models.DateField(auto_now_add=True)
+    expiry_data = models.DateField(null=True)
     
     def __str__(self) -> str:
         return self.name
