3e893337b470bb5c0ba89bbe2256f887e098f5c5
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-27 15:47:35 +0500

    1. Add url of Crud

--- basicCrud/urls.py
+++ basicCrud/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import getData
+from .views import item_list, item_detail
 
 urlpatterns = [
-    path('', getData)
+    path('', item_list),
+    path('item/<int:pk>', item_detail)
 ]
