3bf08b1fd29ed0afb233268deb361f8ea7a6b491
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-28 17:08:24 +0500

    Add more urls of new generic Views

--- basicCrud/urls.py
+++ basicCrud/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import item_list, item_detail, MyItem,ListItem,ReadItem,CreateItem,UpdateItem,DeleteItem
+from .views import item_list, item_detail, MyItem,ListItem,ReadItem,CreateItem,UpdateItem,DeleteItem,ReadCreateItem, ReadSingleUpdateItem, ReadSingleDeleteItem, ReadUpdateDeleteItem
+
 
 urlpatterns = [
     # ------ Function Based View------
@@ -17,7 +18,12 @@ urlpatterns = [
     path('item/<int:pk>/',ReadItem.as_view()),
     path('createitem/',CreateItem.as_view()),
     path('updateitem/<int:pk>/',UpdateItem.as_view()),
-    path('deleteitem/<int:pk>/',DeleteItem.as_view())
+    path('deleteitem/<int:pk>/',DeleteItem.as_view()),
+    
+    path('readcreateitem/', ReadCreateItem.as_view()),
+    path('readsingleupdateitem/<int:pk>/',ReadSingleUpdateItem.as_view()),
+    path('readsingledeleteitem/<int:pk>/',ReadSingleDeleteItem.as_view()),
+    path('readupdatedeleteitem/<int:pk>/',ReadUpdateDeleteItem.as_view())
     
     
     
