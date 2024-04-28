1936b8fc3fb0f8c6a4e762dcd7f1a85dbeff8271
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-28 13:48:36 +0500

    Add class based views urls

--- basicCrud/urls.py
+++ basicCrud/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import item_list, item_detail
+from .views import item_list, item_detail, MyItem
 
 urlpatterns = [
-    path('', item_list),
-    path('item/<int:pk>', item_detail)
+    # ------ Function Based View------
+    # path('', item_list),
+    # path('item/<int:pk>/', item_detail),
+    
+    
+    # ------- Class Based View----
+    path('',MyItem.as_view()),
+    path('item/<int:pk>/', MyItem.as_view()),
+    
+    
+    # ------- Generic View-----
+    path()
+    
+    
+    
 ]
