5c3529a2809a03938325dce60fc62aefba87068e
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-29 13:31:54 +0500

    Register the url of ReadOnlyModelViewSet in router

--- viewsets/urls.py
+++ viewsets/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet, ItemModelViewSet
+from .views import ItemViewSet, ItemModelViewSet, ItemReadOnlyMVSet
 
 # Create Router object
 router = DefaultRouter()
@@ -8,6 +8,7 @@ router = DefaultRouter()
 # Register viewset with Router
 router.register(r'items',ItemViewSet,basename='item')
 router.register(r'modelitems', ItemModelViewSet, basename='modelitem')
+router.register(r'readOnlyMVitems', ItemReadOnlyMVSet, basename='readOnlyMVitem')
 
 
 # Add router url in url patterns
