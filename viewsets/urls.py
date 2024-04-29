b34930e281bd6298a9edcc11bed203e5b6233a15
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-29 13:25:12 +0500

    register the url of model view set in router

--- viewsets/urls.py
+++ viewsets/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet
+from .views import ItemViewSet, ItemModelViewSet
 
 # Create Router object
 router = DefaultRouter()
 
 # Register viewset with Router
 router.register(r'items',ItemViewSet,basename='item')
+router.register(r'modelitems', ItemModelViewSet, basename='modelitem')
+
 
 # Add router url in url patterns
 urlpatterns = router.urls
