e10e55eacf857ef34a238c670c6ec6fe48ba867a
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-30 11:06:25 +0500

    Add Generic View set

--- viewsets/views.py
+++ viewsets/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 
+
 from basicCrud.models import Item
 from basicCrud.serializers import ItemSerializer
 
@@ -48,6 +49,8 @@ class ItemViewSet(viewsets.ViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
         
         
+
+        
 # __________________________________________ Model View Set________________________________
 
 class ItemModelViewSet(viewsets.ModelViewSet):
@@ -61,3 +64,49 @@ class ItemModelViewSet(viewsets.ModelViewSet):
 class ItemReadOnlyMVSet(viewsets.ReadOnlyModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    
+    
+#__________________________________________Generic View Set______________________________
+
+
+class ItemGenericViewSet(viewsets.GenericViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        item = self.get_object()
+        serializer = self.get_serializer(item)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        item = self.get_object()
+        serializer = self.get_serializer(item, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_update(self, request, pk=None):
+        item = self.get_object()
+        serializer = self.get_serializer(item, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        item = self.get_object()
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
