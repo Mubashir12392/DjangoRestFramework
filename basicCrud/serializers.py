72b07d8a21212b5a5c339242833c45e9fa047782
Mubashir12392 <mubashirhaider01@gmail.com>
2024-05-05 22:37:20 +0500

    Change indentation

--- basicCrud/serializers.py
+++ basicCrud/serializers.py
@@ -1,8 +1,9 @@
-from rest_framework  import serializers
+from rest_framework import serializers
 from .models import Item
 
+
 class ItemSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Item
-        fields = '__all__'
+        fields = "__all__"
