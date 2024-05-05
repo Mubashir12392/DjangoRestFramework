d332a3d407b54c6f6041203cc8ca10b46013cafd
Mubashir12392 <mubashirhaider01@gmail.com>
2024-05-05 23:45:44 +0500

    Change Model Name to MyItem

--- basicCrud/models.py
+++ basicCrud/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Item(models.Model):
+class MyItem(models.Model):
 
     name = models.CharField(max_length=50)
     company = models.CharField(max_length=50, null=True)
