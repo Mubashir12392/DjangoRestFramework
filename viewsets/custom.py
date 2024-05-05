ed6c9336163e8aa41488cf62de0134d000f74c8a
Mubashir12392 <mubashirhaider01@gmail.com>
2024-05-05 22:38:34 +0500

    Change indentation

--- viewsets/custom.py
+++ viewsets/custom.py
@@ -1,29 +1,29 @@
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-
-from rest_framework.permissions import IsAdminUser
-
-from rest_framework.authentication import SessionAuthentication 
-from basicCrud.models import Item
-from basicCrud.serializers import ItemSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import (
+    api_view,
+)
 
-@api_view(['GET'])
-# @permission_classes([IsAdminUser])
-# @authentication_classes([SessionAuthentication])
+from basicCrud.models import Item
+from basicCrud.serializers import ItemSerializer
+
+
+@api_view(["GET"])
 def testing_api(request):
-    query_param = int(request.query_params.get('id'))
+    query_param = int(request.query_params.get("id"))
     try:
         item_obj = Item.objects.get(id=query_param)
     except Item.DoesNotExist:
-        return Response({'message': f'Item "{query_param}" not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"message": f'Item "{query_param}" not found'},
+            status=status.HTTP_404_NOT_FOUND,
+        )
     serializer = ItemSerializer(instance=item_obj)
     if serializer.data:
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
-@api_view(['POST'])
+@api_view(["POST"])
 def post_testing_api(request):
     data = request.data
     try:
@@ -32,11 +32,10 @@ def post_testing_api(request):
         if serializer.data:
             return Response(serializer.data)
     except Exception as e:
-        return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-    
 
-@api_view(['PUT'])
+@api_view(["PUT"])
 def update_testing_api(request):
     data = request.data
     try:
@@ -45,32 +44,12 @@ def update_testing_api(request):
             serializer.save()
             return Response(serializer.data)
     except Exception as e:
-        return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['DELETE'])
+@api_view(["DELETE"])
 def delete_testing_api(request):
-    query_param = int(request.query_params.get('id'))
+    query_param = int(request.query_params.get("id"))
     obj = Item.objects.get(id=query_param)
     obj.delete()
-    return Response({'message': 'Record is deleted successfully'})
-
-
-
-from rest_framework.views import APIView
-
-
-class TestingApi(APIView):
-    permission_classes = []
-    authentication_classes = []
-
-    def get(self, request):
-        pass
-    
-    def post(self, request):
-        pass
-    def put(self, request):
-        pass
-
-    def delete(self, request):
-        pass
+    return Response({"message": "Record is deleted successfully"})
